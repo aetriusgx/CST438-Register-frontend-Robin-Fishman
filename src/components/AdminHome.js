b7b5bb3ace3352e9a8b36f9fe79ce7afc2ca987a
Robin Fishman <rfishman@csumb.edu>
2023-09-28 21:14:24 -0700

    Add EditStudent and StudentList

--- src/components/AdminHome.js
+++ src/components/AdminHome.js
@@ -1,25 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import EditStudent from './EditStudent';
 
-const AdminHome = ()  => {
+const AdminHome = () => {
+  const [studentList, setStudentList] = useState([]);
 
-    useEffect(() => {
-        // called once after intial render
-        fetchStudents();
-        }, [] )
-
-
-    const fetchStudents = () => {
-		//TODO complete this method to fetch students and display list of students
-    }
+  useEffect(() => {
+    // called once after intial render
+    fetchStudents();
+  }, [])
 
 
-    return (
-        <div> 
-        <div margin="auto" >
-          <h3>Student List</h3>
-        </div>
+  const fetchStudents = () => {
+    fetch('http://localhost:8080/student/all')
+    .then(res => res.json())
+    .then(data => {
+      setStudentList(data);
+    })
+    .catch(err => console.log(err));
+  }
+
+
+  return (
+    <div>
+      <div margin="auto" >
+        <h3>Student List</h3>
+        {studentList.map(student => <div>
+          {student.email} - {student.name} 
+        </div>)}
+        <BrowserRouter>
+          <div>
+            <Link to='edit'>Edit Student</Link>{' '}
+
+            <Switch>
+              <Route path='/edit' component={EditStudent} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
-    )
+    </div>
+  )
 }
 
 export default AdminHome;
--- src/components/EditStudent.js
+++ src/components/EditStudent.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const EditStudent = (props)  => {
+const EditStudent = (props) => {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('');
+    const [statusCode, setStatusCode] = useState(0);
 
+    let onChangeEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    let onChangeName = (e) => {
+        setName(e.target.value);
+    }
+
+    let onChangeStatus = (e) => {
+        setStatus(e.target.value);
+    }
+
+    let onChangeStatusCode = (e) => {
+        setStatusCode(e.target.value);
+    }
+
+    let handleStudentEdit = () => {
+        fetch('http://localhost:8080/student/edit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                name: name,
+                status: status,
+                statusCode: statusCode
+            }),
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.name !== '') {
+                    console.log("Change successful");
+                }
+            }).catch(err => console.log(err));
+    }
 
     return (
         <div>
-        </div>                       
+            <label>Email: </label>
+            <input type="email" name="email" value={email} onChange={onChangeEmail}></input>
+            <br></br>
+            <label>Edit Name: </label>
+            <input type="text" name="name" value={name} onChange={onChangeName}></input>
+            <br></br>
+            <label>Edit Status: </label>
+            <input type="text" name="status" value={status} onChange={onChangeStatus}></input>
+            <br></br>
+            <label>Edit Status Code: </label>
+            <input type="number" name="status_code" value={statusCode} onChange={onChangeStatusCode}></input>
+            <button onClick={handleStudentEdit}>Save Changes</button>
+        </div>
     )
 }
 
