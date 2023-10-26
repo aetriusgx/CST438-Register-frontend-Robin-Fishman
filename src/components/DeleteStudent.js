722a05109d292dd6017613254701a0e89f0f4dc7
Robin Fishman <rfishman@csumb.edu>
2023-10-25 18:01:14 -0700

    Implement Login component

--- src/components/DeleteStudent.js
+++ src/components/DeleteStudent.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
+import fetchStudents from './AdminHome';
 
 const DeleteStudent = (props) => {
 	const [email, setEmail] = useState('');
+	const [forceDelete, setForceDelete] = useState(true);
 	const [response, setResponse] = useState('');
 
 	let onChangeEmail = (e) => {
 		setEmail(e.target.value);
 	}
 
+	let onChangeForce = (e) => {
+		setForceDelete(e.target.checked);
+	}
+
 	let handleStudentDelete = () => {
-		fetch(`http://localhost:8080/student/${email}?force=true`, {
+		fetch(`http://localhost:8080/student/${email}?force=${forceDelete}`, {
 			method: 'DELETE',
 			headers: {
 				'Content-Type': 'application/json'
@@ -21,6 +27,7 @@ const DeleteStudent = (props) => {
 				}
 			})
 			.catch(err => console.log(err));
+		fetchStudents();
 	}
 
 	return (
@@ -29,6 +36,8 @@ const DeleteStudent = (props) => {
 			<label>Email: </label>
 			<input type="email" name="email" value={email} onChange={onChangeEmail}></input>
 			<br></br>
+			<label>Force Delete </label>
+			<input type="checkbox" name="force" onChange={onChangeForce}></input>
 			<button onClick={handleStudentDelete}>Delete Student</button>
 			<div>{response}</div>
 		</div>
